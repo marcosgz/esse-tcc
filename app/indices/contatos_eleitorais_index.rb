130601d76fd8f7547b5bb7775d880d42b1daba40
Marcos G. Zimmermann <mgzmaster@gmail.com>
2022-01-31 17:27:01 -0300

    Example of how to setup inline mapping, collection, serializer

--- app/indices/contatos_eleitorais_index.rb
+++ app/indices/contatos_eleitorais_index.rb
@@ -1,19 +1,74 @@
 # frozen_string_literal: true
 
 require_relative '../services/csv_reader'
-require_relative 'contatos_eleitorais_index/collections/candidato_collection'
-require_relative 'contatos_eleitorais_index/collections/partido_collection'
-require_relative 'contatos_eleitorais_index/serializers/candidato_serializer'
-require_relative 'contatos_eleitorais_index/serializers/partido_serializer'
-
 class ContatosEleitoraisIndex < Esse::Index
   define_type :candidato do
-    collection Collections::CandidatoCollection
-    serializer Serializers::CandidatoSerializer
+    mapping {
+      "nome": {
+        "type": "string",
+        "index": "analyzed"
+      },
+      "cargo": {
+        "type": "keyword"
+      },
+      "num_legenda": {
+        "type": "short"
+      },
+      "email": {
+        "type": "keyword"
+      }
+    }
+
+    collection do |**context, block|
+      source = context[:source] || File.expand_path('../../../data/contatos_eleitorais.csv', __FILE__)
+      CSVReader.new(source).each_slice(1000) do |batch|
+        block.call batch, **context
+      end
+    end
+
+    serializer do |row, **context|
+      {
+        _id: row['SQ_CANDIDATO'].to_i,
+        name: row['NM_CANDIDATO'],
+        cargo: row['DS_CARGO'],
+        email: row['NM_EMAIL'],
+        num_legenda: row['NR_PARTIDO'],
+      }
+    end
   end
 
   define_type :partido do
-    collection Collections::PartidoCollection
-    serializer Serializers::PartidoSerializer
+    mapping {
+      "nome": {
+        "type": "string",
+        "index": "analyzed"
+      },
+      "sigla": {
+        "type": "keyword"
+      },
+      "num_legenda": {
+        "type": "short"
+      },
+      "email": {
+        "type": "keyword"
+      }
+    }
+
+    collection do |**context, block|
+      source = context[:source] || File.expand_path('../../../data/partidos_politicos_registrados_contato.csv', __FILE__)
+      CSVReader.new(source).each_slice(1000) do |batch|
+        block.call batch, **context
+      end
+    end
+
+    serializer do |row, **context|
+      {
+        _id: row['NR_PARTIDO'].to_i,
+        name: row['NOME'],
+        sigla: row['SIGLA'],
+        email: row['EMAIL'],
+        num_legenda: row['NR_PARTIDO'].to_i,
+      }
+    end
   end
 end
